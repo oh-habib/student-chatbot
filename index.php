d883b51959cdbfed46075751e6ccfb59115346d9
oh-habib <habib125643@gmail.com>
2024-10-24 22:47:13 +0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<title> Web project </title>
+<link rel="stylesheet" href="style.css">
+<head>
+    <body>
+    <section class="description">
+    <p>
+        The "Chatbot" project is a cloud-based student information chatbot system...
+    </p>
+    <div class="button-container">
+        <button class="chatbot-button">Ask the Chatbot</button>
+        <button class="chatbot-button">View FAQs</button>
+        <button class="chatbot-button">Contact Support</button>
+    </div>
+</section>
+
+        <h1>Group members name:</h1>
+       <ol><?php 
+     echo '<li>Harrum Fatima</l>';
+     echo '<li>Umme Habiba</l>';
+    ?> </ol>
+    <h2>Project Name:</h2>
+   <p style="display: inline;">Student Chatbot</p>
+    <img style="display: inline;" src="https://blog.happyfox.com/wp-content/uploads/2020/10/How-AI-Chatbots-are-Transforming-Customer-Service.png"
+    alt="chatbots"> 
+
+<p> <span class="span">Description:</span> The "Chatbot" project  is a cloud-based student information chatbot system.
+It will use artificial intelligence to analyze and understand user queries,
+ will provide appropriate answers. Students can ask questions about college-related activities,
+  and the chatbot will respond accordingly. If a student finds an answer invalid, they can notify the admin,
+   who can then update or delete the incorrect response.</p>
+    </body>
+</head>
+</html>
